4ab939bc138169859a5562ed317e6ade6f32b256
kgarcia18 <kgarcia@scl.edu.gt>
2024-03-01 10:39:05 +0000

    emoji

---
+++ js/kgarcia.js
@@ -0,0 +1,8 @@
+function cuadro(){
+    let kathy = document.querySelector(".kgarcia");
+    let emoji = document.createElement("div"); 
+    kathy.innerHTML = '<div class="emoji">🙁 Kgarcia</div>'
+
+}
+
+cuadro();
